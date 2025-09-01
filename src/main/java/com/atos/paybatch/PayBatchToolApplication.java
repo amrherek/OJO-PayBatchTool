66c3a3a6aaa8a5c736a7b1249936a64cf0c4fc63
amr.herek <amr.herek@gmail.com>
2025-09-01 15:57:00 +0300

    Initial Commit

---
+++ src/main/java/com/atos/paybatch/PayBatchToolApplication.java
@@ -0,0 +1,13 @@
+package com.atos.paybatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayBatchToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayBatchToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atos/paybatch/PayBatchToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.paybatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayBatchToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
